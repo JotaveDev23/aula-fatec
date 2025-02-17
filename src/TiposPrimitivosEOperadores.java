5d5051dddf92b4e9272165967579649666904135
Jotave <jv94443@gmail.com>
2025-02-17 08:50:33 -0300

    aula fatec

---
+++ src/TiposPrimitivosEOperadores.java
@@ -0,0 +1,14 @@
+public class TiposPrimitivosEOperadores {
+    public static void main(String[] args) {
+        //TIPOS PRIMITIVOS DA LINGUAGEM
+        /* int -> valores inteiros
+           double -> valores de ponto fluante (dupla precisão)
+           char -> caracteres ('a' , '2', '.', unicode)
+           boolean -> lógicos (true or false)
+         */
+
+        //OPERADORES
+        // +, -, /, *, %(Resto)
+
+    }
+}
